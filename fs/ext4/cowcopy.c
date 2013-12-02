4d4c4134eb7b58fc32725c9dd533bf27053e0966
Alex Merkulov <am3617@columbia.edu>
2013-12-02 15:08:28 -0800

    checking file system type

--- fs/ext4/cowcopy.c
+++ fs/ext4/cowcopy.c
@@ -7,31 +7,54 @@
 
 asmlinkage int sys_ext4_cowcopy(const char __user *src, const char __user *dest){
 
-//	char *filename;
-//	char *last_slash;
-	//struct path path;
-
+	struct path path;
 	char buf[100];
 	int ret;
+	struct dentry* d;
+	const char *file_system_type;
 
-	//buf = kmalloc(4*sizeof(char),GFP_KERNEL);
 	printk ("**COWCOPY TRIGGERED**\n");
 	ret = copy_from_user(&buf, src, sizeof(buf));
 	printk ("**COWCOPY buffer: %s, ret: %d \n",buf,ret);
-	//kfree(buf);
 
+	kern_path(buf,LOOKUP_FOLLOW,&path);	
 
-//	last_slash = strrchr(src,'/');
-//	if (last_slash != NULL){
-//		filename = last_slash+1;
-//		last_slash = '\0';
-//	}else {
-//		return 0;
-//	}
+	d = path.dentry;
+	//printk("**COWCOPY path: %s\n**",path);
 
-//	printk("**COWCOPY filename:%s**\n",filename);
-	//printk("path:%s\n",src);
-	//kern_path(filename,LOOKUP_FOLLOW,&path);	
+	if (d == NULL){
+		printk("**COWCOPY path.dentry is null**\n");
+		return 0;
+	}
+
+	if (d->d_sb == NULL){
+		printk("**COWCOPY d->d_sb is null**\n");
+		return 0;
+	}
+
+	if ((d->d_sb)->s_type == NULL){
+		printk("**COWCOPY source file is not found. d->d_sb->s_type is null**\n");
+		return 0;
+	}
+
+	if (((d->d_sb)->s_type)->name == NULL){
+		printk("**COWCOPY d->d_sb->s_type->name is null**\n");
+		return 0;
+	}
+
+	file_system_type = ((d->d_sb)->s_type)->name;
+	printk("**COWCOPY: file system type name:%s**\n",file_system_type);
+
+	ret = strncmp(file_system_type,"ext4",strlen(buf));
+
+	if (ret != 0){
+		printk("**COWCOPY file system type doesn't match ext4**\n");
+		return -EOPNOTSUPP;
+	} else {
+			printk("**COWCOPY file system type matches ext4**\n");
+	}
+		
+	
 
 	return 0;
 
