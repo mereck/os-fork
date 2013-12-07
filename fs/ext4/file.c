bbc68fd254522a046300cdd7854b873f3b8c6f06
Alex Merkulov <am3617@columbia.edu>
2013-12-06 20:03:18 -0800

    fixing relative paths, yo

--- fs/ext4/file.c
+++ fs/ext4/file.c
@@ -188,7 +188,7 @@ static int ext4_file_open(struct inode * inode, struct file * filp)
     xattr_ret = ext4_xattr_get( dentry->d_inode ,EXT4_INODE_EXTENTS, "cow" , attributes , sizeof( "1" ));
     //xattr_ret = 0;
     
-    printk("Opening %s/%s num pages %ld xattr_ret %d xattr %s \n" ,parentDentry->d_iname, dentry->d_iname, from_mapping->nrpages, xattr_ret, attributes);
+  //  printk("Opening %s/%s num pages %ld xattr_ret %d xattr %s \n" ,parentDentry->d_iname, dentry->d_iname, from_mapping->nrpages, xattr_ret, attributes);
     *attributes = 0;
 
     if( *attributes && (filp->f_mode & O_WRONLY  || filp->f_mode & O_RDWR)){
