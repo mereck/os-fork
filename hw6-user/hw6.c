0951ab8f3cc1ef225a5dbfc70b8b932de8fe51ec
Alex Merkulov <am3617@columbia.edu>
2013-12-06 16:32:13 -0800

    adding fopen to test

--- hw6-user/hw6.c
+++ hw6-user/hw6.c
@@ -5,16 +5,27 @@
 int main(int argc, char *argv[]) {
 	char *src;
 	char *dest;
+	FILE * fp;
 	src = argv[1];
 	dest = argv[2];
     int ret = test_cowcopy(src,dest);
     if (ret != 0) {
         printf("test cowcopy ret: %d\n", ret);
         printf("Error! Test cowcopy failed!\n");
+		fp = fopen("/data/local/tmp/dest.txt","w");
+
         return 1;
     } else {
 	printf("Launched test_cowcopy with src:%s dest: %s\n",src,dest);
 	}
+
+	
+	//fp = fopen("/data/local/tmp/source.txt","r");
+
+	//fp = fopen("/data/local/tmp/source.txt","r");
+	//fp = fopen("/data/local/tmp/source.txt","r");
+	//fp = fopen("/data/local/tmp/source.txt","r");
+	//fp = fopen("/data/local/tmp/source.txt","r");
 
 	return 0;
 
